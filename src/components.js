326f3518e423991f1dbe8b57323e4d1aefcc7ee9
Lillian Primrose <kyythane@gmail.com>
2020-09-07 00:26:38 -0400

    that didn't work

--- src/components.js
+++
@@ -1,3 +0,0 @@
-export { default as DragHandle } from './components/DragHandle.svelte';
-export { default as DropGroup } from './components/DropGroup.svelte';
-export { default as DropList } from './components/DropList.svelte';
