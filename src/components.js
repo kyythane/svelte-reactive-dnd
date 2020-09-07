a99431ebbffe58e3cb99e3a923287a83bfe3f898
Lillian Primrose <kyythane@gmail.com>
2020-09-07 00:23:34 -0400

    trying something????

---
+++ src/components.js
@@ -0,0 +1,3 @@
+export { default as DragHandle } from './components/DragHandle.svelte';
+export { default as DropGroup } from './components/DropGroup.svelte';
+export { default as DropList } from './components/DropList.svelte';
