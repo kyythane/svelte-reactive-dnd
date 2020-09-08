940ebf492b5332792dae176d8be97b70706cdc8b
Lillian Primrose <kyythane@gmail.com>
2020-09-08 08:40:01 -0400

    Cleanup stories

---
+++ stories/SingleList.stories.js
@@ -0,0 +1,30 @@
+import HorizontalList from './SingleList.svelte';
+
+export default {
+    title: 'Lists/SingleList',
+    component: HorizontalList,
+    argTypes: {
+        numItems: { control: 'number' },
+        direction: {
+            control: 'inline-radio',
+            options: ['horizontal', 'vertical'],
+        },
+    },
+};
+
+const Template = ({ ...args }) => ({
+    Component: HorizontalList,
+    props: args,
+});
+
+export const Basic = Template.bind({});
+Basic.args = {
+    direction: 'vertical',
+    numItems: 5,
+};
+
+export const Horizontal = Template.bind({});
+Horizontal.args = {
+    direction: 'horizontal',
+    numItems: 5,
+};
--- stories/SingleList.svelte
+++ stories/SingleList.svelte
@@ -1,36 +1,49 @@
 <style>
-    .list {
-        height: 200px;
-        width: 100px;
+    .base-list {
         border: solid black 1px;
-        background-color: grey;
+        background-color: lightgray;
     }
-    .item {
+    .vertical-list {
+        height: 400px;
+        width: 200px;
+    }
+    .horizontal-list {
+        height: 58px;
+        width: 500px;
+    }
+    .base-item {
         margin: 2px;
-        width: 78px;
         padding: 0px 8px;
         border: solid black 1px;
-        background-color: burlywood;
+        background-color: whitesmoke;
+    }
+    .vertical-item {
+        width: 178px;
+    }
+    .horizontal-item {
+        width: 78px;
     }
 </style>
 
 <script>
-    import DropList from '../lib/components/DropList.svelte'
+    import DropList from '../lib/components/DropList.svelte';
     import DragHandle from '../lib/components/DragHandle.svelte';
     export let numItems;
+    export let direction;
     let items = new Array(numItems)
         .fill(0)
         .map((_, index) => ({ id: index, title: `Item: ${index}` }));
 </script>
 
-<div class="list">
+<div class="{`base-list ${direction}-list`}">
     <DropList
         items="{items}"
         on:itemdroppedin="{({ detail }) => (items = detail.listSnapshot)}"
+        direction="{direction}"
     >
         <div slot="listItem" let:data="{{ item }}">
             <DragHandle itemId="{item.id}">
-                <div class="item">
+                <div class="{`base-item ${direction}-item`}">
                     <p>{item.title}</p>
                 </div>
             </DragHandle>
